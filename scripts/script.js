9b31f21440eefe86733d6068053460adb89d821a
michaelwhyte <info@mwhyte.ca>
2016-03-08 15:57:22 -0800

    Added files via upload

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+/*
+
+COLOR CHANGER Object
+
+1. Changes the background color
+
+2. Stores all the CSS colors in an array
+
+Color Array from: https://gist.github.com/bobspace/2712980
+
+*/
+var COLOR_CHANGER = {
+
+	// Array of color names
+	colors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"],
+
+	init: function(){
+		
+		var _this = this;
+
+		this.selectColor(this.colors);
+
+		var btn = document.querySelector('.btn-change-color-name');
+
+		// btn.onclick = function(){
+		// 	_this.selectColor(_this.colors);
+		// }
+		btn.addEventListener('click', function(){
+			_this.selectColor(_this.colors);
+		}, false);
+
+	},
+
+	selectColor: function(array){
+		var selectedColor = array[ Math.floor(Math.random() * this.colors.length) ];
+		document.body.style.backgroundColor = selectedColor;
+		document.querySelector('.container-color-name p').innerHTML = selectedColor;
+	} 
+
+}
+
+COLOR_CHANGER.init();
